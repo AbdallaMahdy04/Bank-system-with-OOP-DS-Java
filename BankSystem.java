3e6ac4bf762c90f8cc5758d5a3a2447389dbfcf1
AbdallaMahdy04 <abdallahmahdyam20@gmail.com>
2025-08-17 15:51:38 +0300

    BankSystem_main.java

--- BankSystem.java
+++ BankSystem.java
@@ -3,6 +3,7 @@
  */
 
 package com.mycompany.banksystem;
+import java.util.*;
 
 /**
  *
@@ -11,6 +12,32 @@ package com.mycompany.banksystem;
 public class BankSystem {
 
     public static void main(String[] args) {
+        Bank bank = new Bank("QNB", "110 xyz St", "IFSC123");
+        Admin admin = new Admin("adminX", "password");
+
+        // creating customers
+        admin.addCustomer(bank, "C001", "abdalla", "Address 1", 5000, "Savings");
+        admin.addCustomer(bank, "C002", "karim", "Address 2", 3000, "Current");
+
+        Customer abdalla= bank.searchCustomer("C001");
+        Customer karim = bank.searchCustomer("C002");
+
+        // transactons
+        abdalla.deposit(1000);
+        abdalla.withdraw(500);
+        abdalla.transfer(karim, 2000);
+
+        System.out.println("\nabdalla's Transaction History:");
+        abdalla.displayTransactionHistory();
+
+        System.out.println("\nkarim's Transaction History:");
+        karim.displayTransactionHistory();
        
+        System.out.println("karim account:\n"+karim.toString());
+
+        System.out.println("abdalla acount\n"+abdalla.toString());
+
+
     }
 }
+
